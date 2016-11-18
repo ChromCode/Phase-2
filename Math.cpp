d03b22b9fec82b845056485c6ea5766e0b955c85
yshtalen <yshtalen@ucsc.edu>
2016-11-17 18:04:38 -0800

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -15,6 +15,19 @@ int Math::convert(string s){
         else {return 0;}
 }
 
+string Math::findVar(string var){
+        //true if variable is found in maps, false otherwise
+        bool found = true;
+        //string indicating which map var was found in
+        string found_in="nums";
+        //check that result variable exists in variable map
+        if(createdNUMERICS.find(result_string)==createdNUMERICS.end()){found==false; found_in="real";}
+        else if(createdREALS.find(result_string)==createdREALS.end()){found==false; found_in="none"}
+        //otherwise variable not found
+        if(found==false){cerr << "IN ADD: the variable " << result_string << " does not exist" << endl; found==true;}
+        return found_in;
+}
+
 //checks parameter list, validates type: implemented in derived classes
 bool Math::validator(){}
 //process implemented in derived classes
