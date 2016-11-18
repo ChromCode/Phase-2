4e8341cf79aad5244f42d7f417715c0ecedda5eb
yshtalen <yshtalen@ucsc.edu>
2016-11-17 18:00:56 -0800

    Update Add.cpp

--- Add.cpp
+++ Add.cpp
@@ -14,11 +14,10 @@ bool Add::validator(){
     //cout << "running ADD validator..." << endl;
     //parameter counter
    int num_params=0;
-    // //check that result variable exists in variable map
-    if(createdVariables.find(result_string)==createdVariables.end()){cerr << "IN ADD: the variable " << result_string << " does not exist" << endl; return false;}
+    
+    //find result parameter in maps
+	
     //convert parameter list
-
-
     for(string s : params){
         try{
 	        if(convert(s)!=0){
@@ -26,9 +25,9 @@ bool Add::validator(){
 	        //if parameter is number, convert to float 
 	            if(convert(s)==2) { converted_params.push_back(stod(s)); }
 	            //if parameter is variable, pull value from variable map
-	            else if(convert(s)==1){converted_params.push_back(createdVariables[s]->getNumericValue());}
+	            else if(convert(s)==1){converted_params.push_back(createdVariables[s]->getValue());}
 	        }
-            else{cerr << "IN ADD: bad convert of " << s << endl; return false;}
+            	else{cerr << "IN ADD: bad convert of " << s << endl; return false;}
         } catch(...){return false;}
             //check number of parameters doesn't exceed 12
     }
@@ -47,7 +46,7 @@ void Add::process(){
             createdVariables[result_string]->print();
 
         //set value of first variable to sum of all the rest
-            createdVariables[result_string]->setNumValue(sum);
+            createdVariables[result_string]->setValue(sum);
             cout << "Variable after ADD: ";
             createdVariables[result_string]->print();
     } else {cerr << "IN ADD: invalid parameter list" << endl;}
