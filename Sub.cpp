e55ca4d79915744b93a98640d3196839751ce1cf
yshtalen <yshtalen@ucsc.edu>
2016-11-21 17:13:56 -0800

    Update Sub.cpp

--- Sub.cpp
+++ Sub.cpp
@@ -10,16 +10,28 @@ bool Sub::validator(){
         //cout << "running SUB validator..." << endl;
         //parameter counter
         int num_params=0;
-        //check that result is a variable that exists in the var map
-        if(createdVariables.find(result_string)==createdVariables.end()){cerr << "IN SUB: the variable " << result_string << " does not exist" << endl; return false;}
+        //find result parameter in maps: 0-not found, 1-nums, 2-reals
+        int var_found = findVar(result_string);
+         //variable does not exist in maps
+        if(var_found==0){return false;}
+	
         //convert parameter list
         for(string s : params){
-                if(convert(s)!=0){
-                        num_params++;
-                        if(convert(s)==2){converted_params.push_back(stod(s));}
-                        else if(convert(s)==1){converted_params.push_back(createdVariables[s]->getNumericValue());}
-                }
-                else{cerr << "IN SUB: bad convert of " << s << endl; return false;}
+	        if(convert(s)!=0){
+	                num_params++;
+	                //if parameter is number, convert to float 
+	                if(convert(s)==2) { converted_params.push_back(stod(s)); }
+	                //if parameter is variable, pull value from variable map
+	                else if(convert(s)==1){
+			        int par_var_found = findVar(s);
+	        	        switch(par_var_found){
+				        case 1: converted_params.push_back(createdNUMERICS[s]->getValue());
+				        case 2: converted_params.push_back(createdREALS[s]->getValue());
+				        default: return false;
+			        }
+		        }
+	     }
+             else{cerr << "IN SUB: bad convert of " << s << endl; return false;}
         }
         if(num_params!=2){cerr << "IN SUB: wrong number of parameters" << endl; return false;}
         return true;
@@ -31,12 +43,12 @@ void Sub::process(){
                 //cout << "SUB processing..." << endl;
                 //take the difference
                 float diff = converted_params[0] - converted_params[1];
-                cout << "Variable before SUB: ";
-                createdVariables[result_string]->print();
-
-                createdVariables[result_string]->setNumValue(diff);
-                cout << "Variable after SUB: ";
-                createdVariables[result_string]->print();
+                int var_found = findVar(result_string);
+	        switch(var_found){
+				case 1: createdNUMERICS[result_string]->setValue(sum);
+				case 2: createdREALS[result_string]->setValue(sum);
+				default: return false;
+	        }
         }
         else{cerr << "IN SUB: invalid parameter list" << endl;}
 
