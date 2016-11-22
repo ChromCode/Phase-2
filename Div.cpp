b141fa410c3ddb8103940eb316e9afbd264e0974
yshtalen <yshtalen@ucsc.edu>
2016-11-21 17:10:34 -0800

    Update Div.cpp

--- Div.cpp
+++ Div.cpp
@@ -11,16 +11,28 @@ bool Div::validator(){
         //parameter counter
         int num_params=0;
         //check that result is a variable that exists in the var map
-        if(createdVariables.find(result_string)==createdVariables.end()){cerr << "IN DIV: the variable " << result_string << " does not exist" << endl; return false;}
-        //convert parameter list
-        for(string s : params){
-                cout << "This is real " << s << endl;
-                if(convert(s)!=0){
-                        num_params++;
-                        if(convert(s)==2){converted_params.push_back(stod(s));}
-                        else if(convert(s)==1){converted_params.push_back(createdVariables[s]->getNumericValue());}
-                }
-                else{cerr << "IN DIV: bad convert of " << s << endl; return false;}
+        //find result parameter in maps: 0-not found, 1-nums, 2-reals
+    int var_found = findVar(result_string);
+    //variable does not exist in maps
+    if(var_found==0){return false;}
+	
+    //convert parameter list
+    for(string s : params){
+	    if(convert(s)!=0){
+	        num_params++;
+	        //if parameter is number, convert to float 
+	        if(convert(s)==2) { converted_params.push_back(stod(s)); }
+	        //if parameter is variable, pull value from variable map
+	        else if(convert(s)==1){
+			int par_var_found = findVar(s);
+	        	switch(par_var_found){
+				case 1: converted_params.push_back(createdNUMERICS[s]->getValue());
+				case 2: converted_params.push_back(createdREALS[s]->getValue());
+				default: return false;
+			}
+		 }
+	     }
+             else{cerr << "IN DIV: bad convert of " << s << endl; return false;}
         }
         if(converted_params[1]==0){cerr << "IN DIV: cannot divide by zero!" << endl; return false;}
         if(num_params!=2){cerr << "IN DIV: wrong number of parameters" << endl; return false;}
@@ -34,13 +46,12 @@ void Div::process(){
                 float num = converted_params[0];
                 float den = converted_params[1];
                 float quot = num / den;
-                cout << "Variable before DIV: ";
-                createdVariables[result_string]->print();
-
-                createdVariables[result_string]->setNumValue(quot);
-
-                cout << "Variable after DIV: ";
-                createdVariables[result_string]->print();
+                int var_found = findVar(result_string);
+	        switch(var_found){
+				case 1: createdNUMERICS[result_string]->setValue(sum);
+				case 2: createdREALS[result_string]->setValue(sum);
+				default: return false;
+	    }
         }
         else{cout << "invalid parameter list" << endl;}
 
